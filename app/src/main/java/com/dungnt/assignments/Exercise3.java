28fb61ff770fb390aadf349efc8b2a82b281d34c
DungNt <tandung.nguyen@migitek.com>
2024-09-15 22:51:51 +0700

    [FEATURE]: init ui exercise 3

--- app/src/main/java/com/dungnt/assignments/Exercise3.java
+++ app/src/main/java/com/dungnt/assignments/Exercise3.java
@@ -1,6 +1,8 @@
 package com.dungnt.assignments;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,17 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Exercise3 extends AppCompatActivity {
+    Button btnPlus, btnDivide, btnSub, btnMulti;
+    EditText inputA, inputB, resultEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_exercise3);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        inputA = findViewById(R.id.edta);
+        inputB = findViewById(R.id.edtb);
+        resultEditText = findViewById(R.id.edtc);
+
     }
 }
