f4fa5273de1e687021f7b1c3d5fb47a6dc52b3cb
DungNt <tandung.nguyen@migitek.com>
2024-09-18 22:37:52 +0700

    [FEATURE]: dev ui

--- app/src/main/java/com/dungnt/assignments/baigiang/BaiGiangLayoutHomPage.java
+++ app/src/main/java/com/dungnt/assignments/baigiang/BaiGiangLayoutHomPage.java
@@ -17,10 +17,5 @@ public class BaiGiangLayoutHomPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_bai_giang_layout_hom_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
